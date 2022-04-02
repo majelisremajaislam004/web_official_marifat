4ca98fe2786454ca2a726b18a3ed7d62b1898f15
majelisremajaislam004 <102713199+majelisremajaislam004@users.noreply.github.com>
2022-04-02 10:06:45 +0700

    Add files via upload

---
+++ pages/archive.vue
@@ -0,0 +1,7 @@
+<template>
+    <main>
+        <navbar/>
+
+        <end/>
+    </main>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <main>
+    <navbar/>
+      <div>
+        <hai/>
+      </div>
+    <end/>
+  </main>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      items: '',
+      loading: true,
+    }
+  },
+  mounted() {
+    this.ambilData()
+  },
+  methods: {
+    async ambilData() {
+      const { data,error } = await this.$supabase
+      .form('about')
+      .select()
+      if(data)
+        this.items = data
+        this.loading = false
+      if(error) console.log(error)
+    }
+  }
+}
+</script>
+
